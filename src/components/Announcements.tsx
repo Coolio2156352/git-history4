8f21d9e5dc39da01938539fd5e2292a49f1579de
coolio <coolio2156352@gmail.com>
2024-11-29 20:00:19 +0700

    how are you doing? my $33/hourly client ended the contract on upwork

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -19,7 +19,7 @@ const Announcements = () => {
           <div className="flex items-center justify-between">
             <h2 className="font-medium">Lorem ipsum dolor sit</h2>
             <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
-              2025-01-01
+              2025-01-01-+
             </span>
           </div>
           <p className="text-sm text-gray-400 mt-1">Lorem ipsum dolor sit amet consectetur adipisicing elit dolor sit amet consectetur.</p>
