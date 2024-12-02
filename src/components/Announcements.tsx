53f498f6c0ab305ddf0eb4f43ef4d909e47616d9
coolio <coolio2156352@gmail.com>
2024-12-02 21:35:03 +0700

    poor guy, I hope your luck, maybe the client will die soon

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -9,6 +9,7 @@ const Announcements = () => {
         <div className="bg-lamaSkyLight rounded-md p-4">
           <div className="flex items-center justify-between">
             <h2 className="font-medium">Lorem ipsum dolor sit</h2>
+            <h2 className="font-medium">Lorem ipsum dolor sit</h2>
             <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
               2025-01-01
             </span> 
